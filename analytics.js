cface23e1dcecb41986ca4d8a43abb4667d1da23
محمد الحديدي <mohammed.alhadidi@hotmail.com>
2026-08-23 05:51:23 +0300

    Delete analytics.js

--- analytics.js
+++
@@ -1,9 +0,0 @@
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-VJGN3NQ8WZ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-VJGN3NQ8WZ');
-</script>
