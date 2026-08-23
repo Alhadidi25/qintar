967059518ff8900c175a2d8dde4dc761426e1944
محمد الحديدي <mohammed.alhadidi@hotmail.com>
2026-08-23 05:48:42 +0300

    Add Google Tag Manager script to analytics.js

--- analytics.js
+++ analytics.js
@@ -1,6 +1,9 @@
-
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-VJGN3NQ8WZ"></script>
+<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-VJGN3NQ8WZ');
+</script>
